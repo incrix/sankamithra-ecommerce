b565b23d3cd470b2ac83e4ec87885337c0b668c5
Abishek . A <21ec001@psr.edu.in>
2024-08-21 13:00:47 +0530

    cart

--- app/cart/page.js
+++ app/cart/page.js
@@ -10,7 +10,6 @@ import {
   Paper,
   Button,
   IconButton,
-  ButtonGroup,
 } from "@mui/material";
 import Link from "next/link";
 import { styled } from "@mui/material/styles";
@@ -19,7 +18,7 @@ import { Quicksand } from "next/font/google";
 const quicksand = Quicksand({ subsets: ["latin"] });
 import EmailSubscribe from "../components/emailSubscribe";
 import { Delete } from "@mui/icons-material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -41,24 +40,18 @@ const StyledTableRow = styled(TableRow)(() => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function Cart() {
-  const cartList = localStorage.getItem("cart")
-    ? JSON.parse(localStorage.getItem("cart"))
-    : [];
-  const [cart, setCart] = useState(cartList);
+  // const cartList = localStorage.getItem("cart")
+  //   ? JSON.parse(localStorage.getItem("cart"))
+  //   : [];
+  const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    const cartList = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+    setCart(cartList);
+  }, []);
   const handleIncrement = (index) => {
     const newCart = [...cart];
     newCart[index].count += 1;
